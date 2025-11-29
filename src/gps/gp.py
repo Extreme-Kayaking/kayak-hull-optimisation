08d997533f52bb75c41b1cc128392a0c03219f52
Max <mjvcarroll@gmail.com>
2025-11-29 21:41:49 +0000

    Kernels & GP

---
+++ src/gps/gp.py
@@ -0,0 +1,11 @@
+"""
+Gaussian Process
+"""
+
+def posterior(kernel, data):
+  # posterior distribution
+  return None
+
+def predict(kernel, data, extrapolate):
+  # predict new results
+  return None
---
+++ src/gps/kernel_stability.py
@@ -0,0 +1,14 @@
+"""
+Kernel for the stability (righting moment) curve
+"""
+
+class Kernel:
+  def __init__(self, kayaker_weight, hull, fluid_velocity, hull_velocity):
+    # Initialiser based on constant values
+  
+  def k(self, (theta1, phi1), (theta2, phi2)):
+    """
+      Relate 2d points (theta, phi) where theta is hull (fluid) angle and phi is hull tilt (heel angle) 
+    """
+    # Actual kernel
+    return 0.0
