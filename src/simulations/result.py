695ca001eb0872071b7d3cda99ccce37014a1eb2
Max <mjvcarroll@gmail.com>
2025-12-11 14:32:22 +0000

    Store scenes in simulation results

--- src/simulations/result.py
+++ src/simulations/result.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
+from trimesh import Scene
 
 @dataclass
 class Result:
     """
     float righting_moment: angular force exerted to right the hull
     float draugh_proportion: [0,1] proportion of hull underwater when floating
+    Trimesh.Scene scene: scene containing the tilted hull & waterline for viewing with scene.show()
     """
     righting_moment: float
     draught_proportion: float
+    scene: Scene
